d920f08e3ea78d72cac4f88e6726ac1714f22130
MartinX <martinx2096@gmail.com>
2025-12-09 18:05:07 +0800

    12091805

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 # 最终项目整合的main，如果大家只是验证自己的工程，请只编辑自己的module下的main.py
+# 下面的代码仅测试当前环境能否正常使用torch和CUDA
+import torch
 
-print("a")
+if __name__ == "__main__":
+    print(f"PyTorch 版本: {torch.__version__}")
+    if torch.cuda.is_available():
+        print("CUDA 状态: 可用 (True)")
